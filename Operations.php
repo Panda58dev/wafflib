7b74aab02955d28119b7b2474c29030cdb247015
Panda58dev <panda58.dev@gmail.com>
2021-07-01 14:16:25 +0300

    Fixed a bug with creating a log file

--- Operations.php
+++ Operations.php
@@ -6,9 +6,9 @@ define('ROOT_PATH', __DIR__);
 //Path to log-file
 define('REPORT_FILE', ROOT_PATH.'/log/report_ssh.txt');
 //Checking if the log file exists
-//if(file_exists(REPORT_FILE)==false){
-//    mkdir(ROOT_PATH.'log');
-//}
+if (file_exists(REPORT_FILE) == FALSE) {
+    mkdir(ROOT_PATH.'/log', 0744, TRUE);
+}
 
 
 /**
