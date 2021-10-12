df9d273073cf2a54d8fda2a52a8da08b199df318
Panda58dev <panda58.dev@gmail.com>
2021-10-12 16:42:54 +0300

    Small changes to start a new branch

--- Operations.php
+++ Operations.php
@@ -6,7 +6,7 @@ define('ROOT_PATH', __DIR__);
 //Path to log-file
 define('REPORT_FILE', ROOT_PATH.'/log/report_ssh.txt');
 //Checking if the log file exists
-if (file_exists(REPORT_FILE) == FALSE) {
+if (!file_exists(REPORT_FILE)) {
     mkdir(ROOT_PATH.'/log', 0744, TRUE);
 }
 
