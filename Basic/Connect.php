b15a2c6e292b2a7e6829b9d4e5ff2bd0fa4ce082
Panda58dev <panda58.dev@gmail.com>
2021-10-13 11:26:14 +0300

    Rewrote the connect function

---
+++ Basic/Connect.php
@@ -0,0 +1,42 @@
+<?php
+namespace Wafflib\Basic;
+
+class Connect extends Report
+{
+    public function connect(
+        string $ip, 
+        int $port = 22, 
+        string $name, 
+        string $pass = NULL
+    ) : mixed
+    {
+        //Log entry
+        $this->reportFile('Connect to ['.$ip.':'.$port.'];');
+        //Connect
+        $session = ssh2_connect('ip', 'port');
+
+        //If the connection is not established
+        if (!$session) {
+            //Log entry
+            $this->reportFile('Connection to ['.$ip.':'.$port.'] is failed;');
+            return FALSE;
+        //If the connection is not established
+        } else {
+            //Log entry
+            $this->reportFile('The connection is established!');
+
+            //Log in
+            if (ssh2_auth_password($session, $name, $pass)) {
+                //Log entry
+                $this->reportFile('Log in for the user ('.$name.') on the remote machine;');
+                return $session;
+            } else {
+                //Log entry
+                $this->reportFile("Account log in failed;");
+                return FALSE;
+            }
+        }
+    }
+}
+
+?>
