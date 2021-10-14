4c7eec12ffadf57a84a8d0e45200edd5e4bb56da
Panda58dev <panda58.dev@gmail.com>
2021-10-14 12:20:51 +0300

    Added a method for copying files

---
+++ src/Basic/Copy.php
@@ -0,0 +1,26 @@
+<?php
+namespace Wafflib\Basic;
+
+class Copy {
+
+    //Copying a file to the host machine
+    public function copy($session, $remote_path, $host_path = $_SERVER['USERPROFILE'])
+    {
+        //Log entry
+        Report::reportFile('Copying file ['.$remote_path.'] to the host-machine');
+
+        //Copying using scp
+        if (ssh2_scp_recv($session, $remote_path, $host_path)){
+            //Operation successful
+            //Log entry
+            Report::reportFile('The copy operation was successful!');
+            return TRUE;
+        } else {
+            //Operation failed
+            //Log entry
+            Report::reportFile('The copy operation failed!');
+            return FALSE;
+        }
+    }
+}
+?>
