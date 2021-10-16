e2190b8dc88e9fb1d9468a4b52cdd9a2aa1cb09b
Panda58dev <panda58.dev@gmail.com>
2021-10-16 19:01:52 +0300

    Created a method for sending a file

---
+++ src/Basic/Send.php
@@ -0,0 +1,27 @@
+<?php
+namespace Wafflib\Basic;
+
+class Send
+{
+    //Sending files to remote machine
+    public function send($session, string $host_path, string $remote_path)
+    {
+        //Log entry
+        Report::reportFile('Sending a file ['.$host_path.'] to remote machine');
+
+        //Sending using scp
+        if (ssh2_scp_send($session, $host_path, $remote_path, 0744)) {
+            //Operation successful
+            //Log entry
+            Report::reportFile('The send operation was successful!');
+            return TRUE;
+        } else {
+            //Operation failed
+            //Log entry
+            Report::reportFile('The send operation failed!');
+            return FALSE;
+        }
+    } 
+}
+
+?>
